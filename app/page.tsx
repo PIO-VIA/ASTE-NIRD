aa77d26a22f84ddb732397e07f84a5c9afe706bb
Ngoh1919 <aghet87@gmail.com>
2025-12-05 02:12:40 +0100

    made the presentation page

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const { t } = useTranslation();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" bg-nird-night grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="absolute top-4 right-4">
         <LanguageSwitcher />
       </header>
